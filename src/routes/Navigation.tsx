5ec108c8954ea791f2c7713cc616d714e4be3248
Lucas <lucaspy1943@icloud.com>
2025-08-14 14:48:37 +0200

    El commit anterior es igual a este

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import {
 	BrowserRouter,
 	Route,
@@ -8,51 +9,38 @@ import {
 import logo from "../logo.svg";
 import { routes } from "./routes";
 
-const { to, path, Component, name } = routes[0];
 
-console.log({ to, path, Component, name });
 
 export const Navigation = () => {
 	return (
-		<BrowserRouter>
+		<Suspense fallback={<span>Cargando</span>}>
+			<BrowserRouter>
 			<div className="main-layout">
 				<nav>
 					<img src={logo} alt="React Logo" />
 					<ul>
-						<li>
-							<NavLink
-								to={to}
-								className={({ isActive }) => (isActive ? "nav-active" : "")}
-							>
-								{name}
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to={to}
-								className={({ isActive }) => (isActive ? "nav-active" : "")}
-							>
-								{name}
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to={to}
-								className={({ isActive }) => (isActive ? "nav-active" : "")}
-							>
-								{name}
-							</NavLink>
-						</li>
+						{routes.map(({ to, name }) => (
+							<li key={to}>
+								<NavLink
+									to={to}
+									className={({ isActive }) => (isActive ? "nav-active" : "")}
+								>
+									{name}
+								</NavLink>
+							</li>
+						))}
 					</ul>
 				</nav>
 
 				<Routes>
-					<Route path={path.toString()} element={<Component />} />
-					<Route path={path.toString()} element={<Component />} />
-					<Route path={path.toString()} element={<Component />} />
-					<Route path="/*" element={<Navigate to={to} replace />}></Route>
+					{routes.map(({ Component, path, to }) => (
+						<Route key={to} path={path} element={<Component />} />
+					))}
+
+					<Route path="/*" element={<Navigate to={routes[0].to} replace />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
+		</Suspense>
 	);
 };
