da6545367f2c6de619f58fa798f7e83c9d2e36ca
Lucas <lucaspy1943@icloud.com>
2025-08-15 23:27:01 +0200

    Lazy routes completed

--- src/01-lazyload/layout/LazyLayout.tsx
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -1,9 +1,37 @@
-
+import { Navigate, NavLink, Route, Routes } from "react-router-dom";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
 
 export const LazyLayout = () => {
-  return (
-    <div>Lazy Layout Page</div>
-    
-    // Rutas hijas iran aqui 
-  )
-}
+	return (
+		<div className="lazy-layout">
+			<h2>Lazy Layout</h2>
+			<ul>
+				<li>
+					<NavLink className="nav-link" to="lazyPage1">
+						Lazy Page 1
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className="nav-link" to="lazyPage2">
+						Lazy Page 2
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className="nav-link" to="lazyPage3">
+						Lazy Page 3
+					</NavLink>
+				</li>
+			</ul>
+
+			<Routes>
+				<Route path="lazyPage1" element={<LazyPage1 />} />
+				<Route path="lazyPage2" element={<LazyPage2 />} />
+				<Route path="lazyPage3" element={<LazyPage3 />} />
+
+				<Route path="*" element={<Navigate replace to="lazyPage1" />} />
+			</Routes>
+		</div>
+	);
+};
+
+export default LazyLayout;
